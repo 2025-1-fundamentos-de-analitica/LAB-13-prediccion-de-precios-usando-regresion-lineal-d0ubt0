2ec3156dd952f301af16f45f7e36d17842026d6a
d0ubt <sepabonn@unal.edu.co>
2025-07-11 20:41:23 -0500

    v1

--- homework/homework.py
+++ homework/homework.py
@@ -61,3 +61,110 @@
 # {'type': 'metrics', 'dataset': 'train', 'r2': 0.8, 'mse': 0.7, 'mad': 0.9}
 # {'type': 'metrics', 'dataset': 'test', 'r2': 0.7, 'mse': 0.6, 'mad': 0.8}
 #
+import pandas as pd
+import numpy as np
+import os
+import gzip
+import pickle
+import json
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder, MinMaxScaler
+from sklearn.feature_selection import SelectKBest, f_classif, f_regression
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import GridSearchCV, KFold
+from sklearn.metrics import (
+    make_scorer,
+    r2_score,
+    mean_squared_error,
+    mean_absolute_error,
+)
+
+
+def load_and_clean(path):
+    df = pd.read_csv(path, compression="zip")
+    df["Age"] = 2021 - df["Year"]
+    # df["Age"] = df["Age"] ** 2
+
+    df = df.drop(columns=["Year", "Car_Name"])
+
+    df = df.dropna()
+    X = df.drop(columns=["Present_Price"])
+    y = df["Present_Price"]
+
+    return X, y
+
+
+def save_model_gzip(model, path="files/models/model.pkl.gz"):
+    with gzip.open(path, "wb") as f:
+        pickle.dump(model, f)
+
+
+def save_regression_metrics(
+    model, x_train, y_train, x_test, y_test, path="files/output/metrics.json"
+):
+    metrics = []
+    model = model.best_estimator_
+
+    for X, y, name in [(x_train, y_train, "train"), (x_test, y_test, "test")]:
+        y_pred = model.predict(X)
+        entry = {
+            "type": "metrics",
+            "dataset": name,
+            "r2": r2_score(y, y_pred),
+            "mse": mean_squared_error(y, y_pred),
+            "mad": mean_absolute_error(y, y_pred),
+        }
+        metrics.append(entry)
+
+    with open(path, "w") as f:
+        for entry in metrics:
+            json.dump(entry, f)
+            f.write("\n")
+
+
+x_train, y_train = load_and_clean("files/input/train_data.csv.zip")
+x_test, y_test = load_and_clean("files/input/test_data.csv.zip")
+
+print(x_test.columns)
+print(x_train.head(), y_train.head())
+
+categorical_features = ["Fuel_Type", "Selling_type", "Transmission", "Owner"]
+
+numerical_features = [col for col in x_train.columns if col not in categorical_features]
+
+
+preprocessor = ColumnTransformer(
+    transformers=[
+        ("cat", OneHotEncoder(handle_unknown="ignore"), categorical_features),
+        ("num", MinMaxScaler(), numerical_features),
+    ],
+    remainder="passthrough",
+)
+
+pipeline = Pipeline(
+    [
+        ("preprocessor", preprocessor),
+        ("selector", SelectKBest(f_regression)),
+        ("regressor", LinearRegression()),
+    ]
+)
+
+
+param_grid = {"selector__k": list(range(2, 25))}
+
+cv = KFold(n_splits=10, shuffle=True)
+
+scorer = make_scorer(mean_absolute_error, greater_is_better=False)
+
+grid_search = GridSearchCV(
+    pipeline, param_grid, cv=cv, scoring="neg_mean_squared_error", n_jobs=-1, verbose=4
+)
+
+grid_search.fit(x_train, y_train)
+
+
+print(grid_search.best_params_)
+save_model_gzip(grid_search)
+
+save_regression_metrics(grid_search, x_train, y_train, x_test, y_test)
